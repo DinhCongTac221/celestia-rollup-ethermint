628539546edd2ab439e73c701e57e27fca7fbb12
thinhpn <ngocthinhbk@gmail.com>
2023-05-11 23:32:07 +0700

    add contracts multisend

--- contracts/contracts/token/StandardToken.sol
+++ contracts/contracts/token/StandardToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract StandardToken is ERC20{
+contract StandardToken is ERC20 {
     constructor() ERC20("Simle Token", "STK") {
         _mint(msg.sender, 1_000_000_000 * 10 ** decimals());
     }
---
+++ contracts/contracts/utility/MultiSend.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+interface IERC20 {
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract MultiSend {
+    constructor() {}
+
+    function multiSendFlexibleAmount(
+        IERC20 token,
+        address[] memory to,
+        uint256[] memory amounts
+    ) public {
+        require(to.length == amounts.length, "Invalid input lengths");
+        require(token.balanceOf(msg.sender) >= getTotalAmount(amounts), "Insufficient balance");
+        for (uint256 i = 0; i < to.length; i++) {
+            require(token.transferFrom(msg.sender, to[i], amounts[i]), "Transfer failed");
+        }
+    }
+
+    function multiSendFixedAmount(IERC20 token, address[] memory to, uint256 amount) public {
+        require(token.balanceOf(msg.sender) >= amount, "Insufficient balance");
+        for (uint256 i = 0; i < to.length; i++) {
+            require(token.transferFrom(msg.sender, to[i], amount), "Transfer failed");
+        }
+    }
+
+    function getTotalAmount(uint256[] memory amounts) internal pure returns (uint256) {
+        uint256 total = 0;
+        for (uint256 i = 0; i < amounts.length; i++) {
+            total += amounts[i];
+        }
+        return total;
+    }
+}
