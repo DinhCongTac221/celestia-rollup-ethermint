2408d7938233fa830a701ff800c19be47808141f
thinhpn <ngocthinhbk@gmail.com>
2023-05-15 00:56:04 +0700

    add log

--- server/src/app.js
+++ server/src/app.js
@@ -15,5 +15,6 @@ app.use("/", router);
 
 //running app
 app.listen(config.app.port, async () => {
+    console.log(`Server is running on port ${config.app.port}`);    
     logger.info(`Server is running on port ${config.app.port}`);    
 });
