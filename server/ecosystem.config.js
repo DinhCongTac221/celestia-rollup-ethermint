3f0c992ea7bbcb5bda256d3bd3f7052efc3732c8
thinhpn <ngocthinhbk@gmail.com>
2023-05-15 00:53:48 +0700

    update for deploying server

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,31 +1,13 @@
 module.exports = {
     apps: [
         {
-            name: "Celestia-Api App",
+            name: "Celestia-Ethermint-Api Faucet",
             script: "/usr/bin/yarn",
             args: "app",
             instances: 1,
             exec_mode: "cluster",
             autorestart: true,
             max_memory_restart: "500M",
-        },
-        {
-            name: "Celestia-Api Cron",
-            script: "/usr/bin/yarn",
-            args: "cron",
-            instances: 1,
-            exec_mode: "cluster",
-            autorestart: true,
-            max_memory_restart: "500M",
-        },
-        {
-            name: "Celestia-Api Queue",
-            script: "/usr/bin/yarn",
-            args: "queue",
-            instances: 1,
-            exec_mode: "cluster",
-            autorestart: true,
-            max_memory_restart: "500M",
-        },
+        },        
     ],
 };
